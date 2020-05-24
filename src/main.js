c35e71e8c40770619cf92900dfe50f01d7327a58
Bastien NOEL <bastien.noel@hotmail.fr>
2020-05-24 18:45:30 +0200

    premier prototype de gameplay

--- src/main.js
+++ src/main.js
@@ -1,8 +1,92 @@
-console.log("hello world");
+/** @type {import("../typings/phaser")} */
 
+console.log('%c%s', 'color: #07a71d', "hello world");
 
+const config = {
+    width: 1000,
+    height: 600,
+    type: Phaser.AUTO,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
 
-let game = new Phaser.Game({
-    width: 500,
-    height: 500
-});
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 300
+            },
+            debug: false
+        }
+    },
+}
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('BG', 'assets/img/BG/BG.png');
+    this.load.image('ground', 'assets/img/Tiles/2.png');
+    this.load.image('player', 'assets/img/Object/SnowMan.png');
+    this.load.image('igloo1', 'assets/img/Object/Igloo1.png');
+    this.load.image('igloo2', 'assets/img/Object/Igloo2.png');
+
+}
+
+function create() {
+    bg = this.add.image(500, 250, 'BG');
+    bg.setScale(0.6, 0.6);
+
+    platforms = this.physics.add.staticGroup({
+        key: 'ground',
+        repeat: 11,
+        setXY: {
+            x: 60,
+            y: 540,
+            stepX: 127
+        }
+    });
+
+    player = this.physics.add.sprite(500, 300, 'player');
+    player.setBounce(0.2);
+    player.setScale(0.5, 0.5);
+    player.body.setGravityY(300)
+    player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, platforms);
+
+    igloo1 = this.physics.add.sprite(870, 427, 'igloo1');
+    igloo1.setScale(0.5, 0.5);
+    this.physics.add.collider(igloo1, platforms);
+    this.physics.add.collider(igloo1, player);
+    igloo1.setCollideWorldBounds(true);
+
+    igloo2 = this.physics.add.sprite(130, 427, 'igloo2');
+    igloo2.setScale(0.5, 0.5);
+    this.physics.add.collider(igloo2, platforms);
+    this.physics.add.collider(igloo2, player);
+    igloo2.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-300);
+
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(300);
+
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+
+}
